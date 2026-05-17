732a26af10115cc7e7283c3321ea99266cb14cc1
Nantoumé_dev <djenebanantoume44@gmail.com>
2026-05-17 15:45:31 +0000

    Correction requirements

--- Jobconnect/settings.py
+++ Jobconnect/settings.py
@@ -6,11 +6,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
