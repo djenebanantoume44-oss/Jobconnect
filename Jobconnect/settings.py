a7594f346acf97c80d899269419e807a1a34e8e9
Nantoumé_dev <djenebanantoume44@gmail.com>
2026-05-19 20:02:16 +0000

    debug true

--- Jobconnect/settings.py
+++ Jobconnect/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = config('SECRET_KEY', default='cle-temporaire-pour-le-build')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com', '.railway.app']
 
